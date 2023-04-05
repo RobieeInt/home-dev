a716e9192a4668ba9ebb576cf0ec73f430b337e5
Robby Hernowo <robbyhernowo@gmail.com>
2023-04-06 00:25:17 +0700

    image city

--- database/migrations/2023_03_19_065301_create_master_cities_table.php
+++ database/migrations/2023_03_19_065301_create_master_cities_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->nullable();
+            $table->string('image')->nullable();
             $table->foreignId('province_id')->constrained('master_provinces');
             $table->timestamps();
             $table->softDeletes();
